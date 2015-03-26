4afedd7fe3abaf9099223f2faceedd4b39293bce
YuriSS <yuri.s.dos.santos@gmail.com>
2015-03-25 23:34:21 -0300

    Inicio do projeto

---
+++ README.md
@@ -0,0 +1,7 @@
+# Algoritmo de Pesquisa Binária
+
+Um algoritmo de Pesquisa Binrária desenvolvido em C.
+
+Descrição do exercício:
+
+A tarefa consiste na implementação do algoritmo de pesquisa binária explicado em aula. Você deve ler um arquivo de palavras (anexo), armazená-las em um vetor e realizar a busca por algumas delas. A pesquisa deve retornar a linha em que a palavra se encontra no arquivo, bem como o número de acessos necessários para que fosse encontrada. Caso a palavra não esteja presente, deve-se retornar 0.
---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+
+int PB(char lista[29858][200], char palavra[200], int inicio, int fim, int* times) {
+	if(inicio < fim) {
+		float calc = (fim + inicio) / 2;
+		int centro = (int)calc;
+		if(strcmp(palavra, lista[centro]) == 0) {
+			return centro + 1;
+		} else if(strcmp(palavra, lista[centro]) < 0) {
+			times++;
+			fim = centro - 1;
+			return PB(lista, palavra, inicio, fim, times);
+		} else {
+			times++;
+			inicio = centro + 1;
+			return PB(lista, palavra, inicio, fim, times);
+		}
+	} else {
+		return 0;
+	}
+}
+
+void toUpperCase(char string[200]) {
+	int i = 0;
+	char ch;
+	while(string[i]) {
+		ch = toupper(string[i]);
+		string[i] = ch;
+		i++;
+	}
+}
+
+int main() {
+
+	char texto[29858][200];
+	char palavra[200];
+	int i = 0;
+	int times = 0;
+	FILE* file = fopen("palavras.txt", "r");
+	while(fscanf(file, "%s", texto[i]) != EOF) {
+		i++;
+	}
+
+	printf("Escreva uma palavra para ser encontrada no texto:\n> ");
+	scanf("%s", palavra);
+
+	toUpperCase(palavra);
+
+	i--;
+	int busca = PB(texto, palavra, 0, i, &times);
+
+	if(busca > 0)
+		printf("\nPalavra (%s) encontrada na linha %d com %d tentativas\n\n", palavra, busca, times);
+	else
+		printf("\nPalavra (%s) nao encontrada...\n\n", palavra);
+
+	return 0;
+
+}
